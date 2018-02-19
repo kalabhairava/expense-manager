6622f864d7bfc01513be92da9c09c0f3a6d38ccc
Manjunatha D <manjunathad13@gmail.com>
2018-02-19 23:42:03 +0530

    Configure react-router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import App from './components/App';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter>
+		<div>
+			<Link to="/create"> Create </Link>
+			<Route path="/" component={App} exact />
+			<Route path="/create" component={Dummy} />
+		</div>
+	</BrowserRouter>,
+	document.getElementById('root')
+);
+
+function Dummy(props) {
+	return <p> Boom </p>;
+}
