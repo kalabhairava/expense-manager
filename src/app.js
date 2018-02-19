801c95800c4772a9ca4d49b268017973906fbd3b
Manjunatha D <manjunathad13@gmail.com>
2018-02-19 23:46:41 +0530

    Configure 404

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import App from './components/App';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(
 	<BrowserRouter>
-		<div>
-			<Link to="/create"> Create </Link>
+		<Switch>
 			<Route path="/" component={App} exact />
 			<Route path="/create" component={Dummy} />
-		</div>
+			<Route component={PageNotFound} />
+		</Switch>
 	</BrowserRouter>,
 	document.getElementById('root')
 );
 
 function Dummy(props) {
 	return <p> Boom </p>;
+}
+
+function PageNotFound() {
+	return <div>Page not found.</div>;
 }
